3d257cad8fa0eec78c3769271c59a25847fd03c6
Kristina Peru <krissy.germanov@gmail.com>
2022-05-03 13:29:25 -0600

    Shows dreams in list email

--- app/mailers/list_mailer.rb
+++ app/mailers/list_mailer.rb
@@ -2,13 +2,14 @@ class ListMailer < ApplicationMailer
     default from: 'beforeidieapplication@gmail.com'
 
     def send_list
-        @sending_list = params[:sending_list]
-        puts params.inspect
         @user = params[:user]
+        @sending_list = params[:sending_list]
+        @list_items = @sending_list.dreams.map {|item| item.dream}
         @url = 'https://beforeidieapp.herokuapp.com/'
         mail(
+            from: "Before I Die | A Bucket List App",
             to: @user.email, 
-            subject: "Test email"
+            subject: "You #{@sending_list.title} Bucket List "
         )
     end
 end
